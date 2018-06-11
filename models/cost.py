4937ff6f8e6f3c24a72e8a492ab1adc6c340088b
César Gtz <cesareomargtz@gmail.com>
2018-06-11 10:42:49 -0600

    insert calculate cost

--- models/cost.py
+++ models/cost.py
@@ -17,15 +17,32 @@ class ReportCalculateCost(models.AbstractModel):
         self.model = self.env.context.get('active_model')
         docs = self.env[self.model].browse(self.env.context.get('active_id'))
         date_range = pd.date_range(docs.date_start, docs.date_end)
-        bills_purchase = self.get_invoice(['in_invoice', 'in_refund'], date_range)
+        get_bills = self.get_invoice(['in_invoice', 'in_refund'], date_range)
+        bills = get_bills[0]
+        bills_sale = get_bills[1]
+        union = set(bills.keys()) | set(bills_sale.keys())
+        keys_ordes = self.get_keys_order(union)
+        dir_totals = self.get_totals()
+        docargs = {
+            'doc_ids': self.ids,
+            'doc_model': self.model,
+            'docs': docs,
+            'expand_invoices': docs.expand_invoices,
+            'dir': bills,
+            'sales': bills_sale,
+            'keys_order': keys_ordes,
+            'totals': dir_totals
+        }
+        return self.env['report'].render('cost_calculate.report_cost', docargs)
 
     @api.multi
     def get_invoice(self, types, date_range):
-        dir_dates = {}
+        purchases_invoices, sales_invoices = {}, {}
         for single_date in date_range:
             tons = amount = cost = init_ton = init_amount = 0
             date = single_date.strftime("%Y-%m-%d")
-            bills = self.reorganize(date, types, "sum_totals") #Purchases
+            bills = self.reorganize(date, types) #Purchases
+            dir_bill_purchase = bills[2]
             last_cost = self.env['initial.inv'].search([], order='date desc', limit=1)
             init_ton = bills[0] + last_cost.tons_daily
             init_amount = bills[1] + last_cost.amount_daily
@@ -35,17 +52,30 @@ class ReportCalculateCost(models.AbstractModel):
             if date == inv.initial_year:
                 init_ton += inv.initial_tons
                 init_amount += inv.initial_import
-            bills = self.reorganize(date, ['out_invoice', 'out_refund'], "sum_totals") #Sales
-            init_ton -= bills[0]
-            init_amount -= bills[1]
-            self.insert_inv(init_amount, init_ton, cost, date)
+            bills_sale = self.reorganize(date, ['out_invoice', 'out_refund']) #Sales
+            dir_bill_sale = bills_sale[2]
+            init_ton -= bills_sale[0]
+            init_amount -= bills_sale[1]
+            self.insert_inv(init_amount, init_ton, cost, date, bills[3]['tpd'], bills[3]['tpdr'],
+                            bills[3]['apd'],bills[3]['apdr'],bills[3]['tsd'],bills[3]['tsdr'],
+                            bills[3]['asd'],bills[3]['asdr'])
+            # Directorio de facturas
+            if dir_bill_purchase:
+                if not date in purchases_invoices:
+                    purchases_invoices.update({ str(date): {} })
+                purchases_invoices[str(date)] = dir_bill_purchase
+            if dir_bill_sale:
+                if not date in sales_invoices:
+                    sales_invoices.update({ str(date): {} })
+                sales_invoices[str(date)] = dir_bill_sale
+        return [purchases_invoices, sales_invoices] #Return dir of purchases and sales invoice
 
-    def reorganize(self, date, types, response):
-        tons, amount = 0, 0
+    def reorganize(self, date, types):
+        tons, amount, dir_keys_prd = 0, 0, {}
+        daily = {'tpd': 0, 'tpdr': 0, 'apd': 0, 'apdr': 0,
+            'tsd': 0, 'tsdr': 0, 'asd': 0, 'asdr': 0}
         bills = self.bills(date, types)
         for bill in bills:
-            # if not str(bill.date_invoice) in dir_dates:
-            #     dir_dates.update({str(bill.date_invoice):{}})
             product_check = self.env['account.invoice.line'].search([('invoice_id', '=', bill.id)])
             for product in product_check:
                 ppp = product.product_id.product_tmpl_id
@@ -54,35 +84,79 @@ class ReportCalculateCost(models.AbstractModel):
                     if bill.type in ['in_invoice', 'in_refund']:
                         if bill.type == types[0]:
                             amount += bill.amount_total
+                            daily['apd'] += bill.amount_total
                             if (ppp.sum_tons_cost):
                                 tons += product.quantity
+                                daily['tpd'] += product.quantity
                         # Resta las devolucione
                         elif bill.type == types[1]:
                             amount -= bill.amount_total
+                            daily['apdr'] += bill.amount_total
                             if (ppp.sum_tons_cost):
                                 tons -= product.quantity
+                                daily['tpdr'] += product.quantity
                     else:
                         if (ppp.sum_tons_cost):
                             if bill.type == types[0]:
                                 amount += bill.amount_total
                                 tons += product.quantity
+                                daily['asd'] += bill.amount_total
+                                daily['tsd'] += product.quantity
                             elif bill.type == types[1]:
                                 amount -= bill.amount_total
                                 tons -= product.quantity
-        if response == "sum_totals":
-            return [tons, amount]
-        elif respone == "get_dir":
-            pass
+                                daily['asdr'] += bill.amount_total
+                                daily['tsdr'] += product.quantity
+                    obj = {
+                        'number': bill.number,
+                        'parner': bill.partner_id.name,
+                        'date': bill.date_invoice,
+                        'product': ppp.name,
+                        'tons':  product.quantity,
+                        'precio_unitario': product.price_unit,
+                        'total': bill.amount_total,
+                        'type': bill.type,
+                        'sum_tons': ppp.sum_tons_cost
+                    }
+                    if not str(ppp.id) in dir_keys_prd:
+                            dir_keys_prd.update({str(ppp.id): []})
+                    dir_keys_prd[str(ppp.id)].append(obj)
+        return [tons, amount, dir_keys_prd, daily]
+
 
     def bills(self, single_date, typeinv):
             return self.env['account.invoice'].search(
                             [('type', 'in', typeinv), ('date_invoice', '=', single_date),
                             ('state', 'in', ['open', 'paid'])], order="date_invoice")
 
-    def insert_inv(self, amount, tons, cost, date):
+    def insert_inv(self, amount, tons, cost, date, tpd, tpdr,
+                   apd, apdr, tsd, tsdr, asd, asdr):
         self.env['initial.inv'].create({
                         'amount_daily': amount,
                         'tons_daily': tons,
                         'cost_daily': cost,
-                        'date': date
+                        'date': date,
+                        'tpd': tpd,
+                        'tpdr': tpdr,
+                        'apd': apd,
+                        'apdr': apdr,
+                        'tsd': tsd,
+                        'tsdr': tsdr,
+                        'asd': asd,
+                        'asdr': asdr
                     })
+
+    def get_keys_order(self, list):
+            ks = sorted(list, key=lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'), reverse=True)
+            return ks
+
+    # def get_totals(self,date_start, date_end):
+    #     dir_totals = {}
+    #     totals = self.env['initial'].search([('date','>=',date_start),
+    #                                          ('date','<=',date_end)], order="date desc")
+    #     for total in totals:
+    #         if total.date in dir_total:
+    #             dir_totals.update({str(total.date): {
+    #                                 'sum_tons': ,
+    #                                 'sum_import',
+    #             }})
--- models/initial_inv.py
+++ models/initial_inv.py
@@ -10,7 +10,18 @@ class Truck(models.Model):
     name = fields.Char( required=True, select=True, copy=False, default=lambda
         self: self.env['ir.sequence'].next_by_code('code_daily_cost'), help="Unique number")
 
+    # Sumatoria día a día
     amount_daily = fields.Float()
     tons_daily = fields.Float()
     cost_daily = fields.Float()
     date = fields.Date()
+
+    # Diario
+    tpd = fields.Float(string="Tns compra")
+    tpdr = fields.Float(string="Dev tons compra")
+    apd = fields.Float(string="Monto compra")
+    apdr = fields.Float(string="Monto de NC compra")
+    tsd = fields.Float(string="Tns venta")
+    tsdr = fields.Float(string="Tns de NC venta")
+    asd = fields.Float(string="Monto de venta")
+    asdr = fields.Float(string="Dev tons venta")
